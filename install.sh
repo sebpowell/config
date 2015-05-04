02171ffe878aae5661346ed39bb425b9e45ff8ea
Sebastien Powell <sebastienpowell@gmail.com>
2015-05-04 12:59:31 +0100

    Added rails and RVM

--- install.sh
+++ install.sh
@@ -9,14 +9,21 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 brew install caskroom/cask/brew-cask # Makes it easier to install mac apps
 
+### Install RVM & Rails
+curl -sSL https://get.rvm.io | bash -s stable --rails
+
 ### Install ZShell
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 ### Makes it easier to update when installed via Homebrew
 brew install git
-brew install ruby
 brew install node
-brew install scala
+brew install pow # Easy proxying
+brew install imagemagick
+
+# Front-end development
+sudo gem install sass
+sudo npm install gulp -g
 
 ### Core apps
 brew cask install --appdir="~/Applications" spectacle
@@ -33,19 +40,14 @@ brew cask install --appdir="~/Applications" dashlane
 brew cask install --appdir="~/Applications" gitx
 brew cask install --appdir="~/Applications" java
 brew cask install --appdir="~/Applications" dropbox
+brew cask install --appdir="~/Applications" postgres
 brew cask install --appdir="~/Applications" mamp
 brew cask install --appdir="~/Applications" postgres
 brew cask install --appdir="~/Applications" sequel-pro
 brew cask install --appdir="~/Applications" font-explorer-x
-
-### Other essentials
-brew install pow # Easy proxying
-brew install imagemagick
+brew cask install --appdir="~/Applications" flux
 
 # cleanup
 brew cleanup --force
 rm -f -r /Library/Caches/Homebrew/*
 
-# Front-end development
-sudo gem install sass
-sudo npm install gulp -g
